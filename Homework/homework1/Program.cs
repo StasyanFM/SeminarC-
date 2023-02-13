82ee41b55dcb64dcafbe0f8889fc94ede1ecdff9
Стасян <stas.baranovfm@gmail.com>
2023-02-13 23:50:30 +0300

    fourth homework assignment

--- Homework/homework1/Program.cs
+++ Homework/homework1/Program.cs
@@ -1,23 +1,47 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// // a = 5; b = 7 -> max = 7
+// // a = 2 b = 10 -> max = 10
+// // a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"Number {num1} more than number {num2}");
+// }
+// else if (num1 < num2)
+// {
+//     Console.WriteLine($"Number {num2} more than number {num1}");
+// }
+// else
+// {
+//     Console.WriteLine($"Number {num1} equals number {num2}");
+// }
+// _____________________________________________________________________________________________________________________
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 Console.WriteLine("Input first number");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second number");
 int num2 = Convert.ToInt32(Console.ReadLine());
-
-if (num1 > num2)
+Console.WriteLine("Input third number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) 
 {
-    Console.WriteLine($"Number {num1} more than number {num2}");
+    max = num2;
 }
-else if (num1 < num2)
+if (num3 > max) 
 {
-    Console.WriteLine($"Number {num2} more than number {num1}");
+    max = num3;
 }
-else
-{
-    Console.WriteLine($"Number {num1} equals number {num2}");
-}
+Console.WriteLine($"Число {max} наибольшее из введенных чисел");
