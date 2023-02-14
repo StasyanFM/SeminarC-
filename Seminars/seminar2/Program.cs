ce49db8a157d8ae20703a45ff5551f78fa985f9b
Стасян <stas.baranovfm@gmail.com>
2023-02-14 20:32:39 +0300

    solution of the second task

--- Seminars/seminar2/Program.cs
+++ Seminars/seminar2/Program.cs
@@ -6,20 +6,43 @@
 
 // 85 -> 8
 
-void MaxDecimal(int num)
+// void MaxDecimal(int num)
+// {
+//     int ed = num % 10;
+//     int dec = num / 10;
+//     if (ed > dec)
+//     {
+//         Console.WriteLine($"Большая цифра числа {num} -> {ed}"); // если метод был бы int то нужно писать return ed;
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Большая цифра числа {num} -> {dec}"); // если метод был бы int то нужно писать return dec;
+//     }
+// }
+
+// int randomnumber = new Random().Next(10, 99 + 1);
+
+// MaxDecimal(randomnumber);
+// ______________________________________________________________________________________________________________________
+
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+
+// 782 -> 72
+
+// 918 -> 98
+
+int DeleteDecimal(int num)
 {
+    int sot = num / 100;
     int ed = num % 10;
-    int dec = num / 10;
-    if (ed > dec)
-    {
-        Console.WriteLine($"Большая цифра числа {num} -> {ed}"); // если метод был бы void то нужно писать return ed;
-    }
-    else
-    {
-        Console.WriteLine($"Большая цифра числа {num} -> {dec}"); // если метод был бы void то нужно писать return dec;
-    }
+    return sot * 10 + ed;
 }
 
-int randomnumber = new Random().Next(10, 99 + 1);
+int randomnumber = new Random().Next(100, 1000);
+Console.WriteLine(randomnumber);
 
-MaxDecimal(randomnumber);
+// int result = DeleteDecimal(randomnumber);
+// Console.WriteLine(result);
+Console.WriteLine(DeleteDecimal(randomnumber));
