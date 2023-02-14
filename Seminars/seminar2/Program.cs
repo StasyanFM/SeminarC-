231f38080544496ee40bbd5da6df60e2dc90b071
Стасян <stas.baranovfm@gmail.com>
2023-02-14 20:20:56 +0300

    solution of the first task

--- Seminars/seminar2/Program.cs
+++ Seminars/seminar2/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Работает Да");
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// 78 -> 8
+
+// 12-> 2
+
+// 85 -> 8
+
+void MaxDecimal(int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    if (ed > dec)
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {ed}"); // если метод был бы void то нужно писать return ed;
+    }
+    else
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {dec}"); // если метод был бы void то нужно писать return dec;
+    }
+}
+
+int randomnumber = new Random().Next(10, 99 + 1);
+
+MaxDecimal(randomnumber);
