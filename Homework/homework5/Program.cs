19b2d21e02ac2d6ba89d4dc6d7b05bb2e82f8744
Стасян <stas.baranovfm@gmail.com>
2023-03-01 22:02:03 +0300

    3s save

--- Homework/homework5/Program.cs
+++ Homework/homework5/Program.cs
@@ -20,18 +20,18 @@ void ShowArray(int[] array)
     Console.WriteLine();
 }
 
-void EvenNumber(int[] array)               
-{
-    int temp = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] % 2 == 0)
-        {
-            temp +=1;
-        }
-    }
-    Console.WriteLine($"Четных чисел {temp}");
-}
+// void EvenNumber(int[] array)               
+// {
+//     int temp = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0)
+//         {
+//             temp +=1;
+//         }
+//     }
+//     Console.WriteLine($"Четных чисел {temp}");
+// }
 
 Console.WriteLine("Введите размер массива:");
 int size = Convert.ToInt32(Console.ReadLine());
@@ -40,12 +40,26 @@ int minValue = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальный элемент массива:");
 int maxValue = Convert.ToInt32(Console.ReadLine());
 
-int[] myArray = CreatRandomArray(size, minValue, maxValue);
-ShowArray(myArray);
-EvenNumber(myArray);
+// int[] myArray = CreatRandomArray(size, minValue, maxValue);
+// ShowArray(myArray);
+// EvenNumber(myArray);
 // __________________________________________________________________________________________________________________
 
 // Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+void SumNumber(int[] array)               
+{
+    int temp = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+         temp = temp + array[i];       
+    }
+    Console.WriteLine($"Сумма числе на нечетных позициях {temp}");
+}
+
+int[] myArray = CreatRandomArray(size, minValue, maxValue);
+ShowArray(myArray);
+SumNumber(myArray);
+// ____________________________________________________________________________________________________________________
