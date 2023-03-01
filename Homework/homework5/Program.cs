a9a06313f855d68a3feddfc3a6aa648440c9e60e
Стасян <stas.baranovfm@gmail.com>
2023-03-01 21:38:32 +0300

    2s save

--- Homework/homework5/Program.cs
+++ Homework/homework5/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-
-
 int[] CreatRandomArray(int size, int minValue, int maxValue)
 {
     int[] array = new int[size];    // выделение памяти под массив
@@ -22,7 +20,7 @@ void ShowArray(int[] array)
     Console.WriteLine();
 }
 
-void CountNumber(int[] array)               
+void EvenNumber(int[] array)               
 {
     int temp = 0;
     for (int i = 0; i < array.Length; i++)
@@ -44,6 +42,10 @@ int maxValue = Convert.ToInt32(Console.ReadLine());
 
 int[] myArray = CreatRandomArray(size, minValue, maxValue);
 ShowArray(myArray);
-CountNumber(myArray);
+EvenNumber(myArray);
+// __________________________________________________________________________________________________________________
 
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
