56020844e40c8e4b51f8584e89bfae4a1b613107
Стасян <stas.baranovfm@gmail.com>
2023-03-10 01:52:13 +0300

    2s save

--- Homework/homework7/Program.cs
+++ Homework/homework7/Program.cs
@@ -52,14 +52,84 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-int [,] Create2DRandomArray(int colums, int rows, int minValue, int maxValue)
+// int[,] Create2DRandomArray(int rows, int colums, int minValue, int maxValue)
+// {
+//     int[,] newArray = new int[rows, colums];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colums; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue + 1, maxValue + 1);
+
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// void ChangeElement(int[,] array, int row, int colum)
+// {
+//     {
+//         row--;
+//         colum--;
+//         if (colum > array.GetLength(0) && row > array.GetLength(1)) // почему если напишу if (row > array.GetLength(0) && colum > array.GetLength(1)) и при выполнении данного условия не срабатывает предупреждение 
+//         {
+//             Console.WriteLine($"Элемента в строке {row + 1} и столбце {colum + 1} -> не существует");
+//         }
+//         else
+//         {
+//             object temp = array.GetValue(row, colum); // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL. 
+//             Console.WriteLine($"Элемент в строке {row + 1} и столбце {colum + 1} -> {temp}");
+//         }
+//     }
+// }
+
+// Console.WriteLine("Введите количество строк:");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int colums = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальный элемент массива: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальный элемент массива: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите строку необходимого элемента: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите столбец необходимого элемента: ");
+// int colum = Convert.ToInt32(Console.ReadLine());
+
+
+// int[,] myArray = Create2DRandomArray(rows, colums, minValue, maxValue);
+// Show2DArray(myArray);
+// ChangeElement(myArray, row, colum);
+// _________________________________________________________________________________________________________________________________________________________________________________
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] Create2DRandomArray(int rows, int colums, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, colums];
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < colums; j++)
         {
-            newArray[i, j] = new Random().Next(minValue, maxValue+1);
+            newArray[i, j] = new Random().Next(minValue, maxValue);
 
         }
     }
@@ -78,3 +148,29 @@ void Show2DArray(int[,] array)
     }
     Console.WriteLine();
 }
+
+void ChangeAverage(int[,] array)
+{        
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            average = average + array[i, j];
+        }
+            Console.Write($"{average / array.GetLength(0)}; ");
+    }
+}
+
+Console.WriteLine("Введите количество строк:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальный элемент массива: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальный элемент массива: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = Create2DRandomArray(rows, colums, minValue, maxValue);
+Show2DArray(myArray);
+ChangeAverage(myArray);
